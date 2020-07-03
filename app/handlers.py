e4a28657677a03c2987e03f4de51c43b442090c6
Egor14 <egorgrekhnev@mail.ru>
2020-07-03 16:43:11 +0300

    del logs

--- app/handlers.py
+++ app/handlers.py
@@ -15,13 +15,13 @@ from settings_local import TIME_INTERVAL
 def rates():
     xqc_rate = db.session.query(XqcRate).order_by(XqcRate.timestamp.desc()).first()
     if xqc_rate:
-        print('\nrate object', xqc_rate.__dict__, flush=True)
+        # print('\nrate object', xqc_rate.__dict__, flush=True)
 
         rates = []
         rates.append({'code': 'USD', 'name': 'US Dollar', 'rate': xqc_rate.usd_rate})
         rates.append({'code': 'JPY', 'name': 'Japanese Yen', 'rate': xqc_rate.jpy_rate})
 
-        print('rates', rates, flush=True)
+        # print('rates', rates, flush=True)
 
         return jsonify(rates)
 
@@ -31,12 +31,12 @@ def rates():
 @app.route('/api/v1/historical-rates/<currency>')
 def historical_rates(currency):
     request_ts = int(request.args.get('ts'))
-    print('\nnew request', request_ts, currency, flush=True)
+    # print('\nnew request', request_ts, currency, flush=True)
 
     start_ts = request_ts // 1000 - TIME_INTERVAL
     end_ts = request_ts // 1000 + TIME_INTERVAL
-    print('start_ts', start_ts, flush=True)
-    print('end_ts', end_ts, flush=True)
+    # print('start_ts', start_ts, flush=True)
+    # print('end_ts', end_ts, flush=True)
     ts_rates = XqcRate.query.filter(XqcRate.timestamp > start_ts, XqcRate.timestamp < end_ts).order_by(
         XqcRate.timestamp.desc()).first()
     if ts_rates:
@@ -53,7 +53,7 @@ def historical_rates(currency):
             'rate': rate
         }
 
-        print('result', result, flush=True)
+        # print('result', result, flush=True)
 
         return jsonify(result)
 
